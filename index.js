d0eed6de745c11ff260298c5655fe5792774a4d3
Alex <slicer256@gmail.com>
2019-06-12 16:19:20 +0300

    eslint fixes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const config = {
   timeout: argv.t || 5,
-  delta: argv.d || 1,
-}
+  delta: argv.d || 1
+};
 
 let isRunning = false;
 
@@ -14,17 +14,17 @@ if (config.timeout < config.delta) {
 }
 
 const currentTime = () => {
-  time = new Date(new Date().toUTCString());
+  const time = new Date(new Date().toUTCString());
   return time;
-}
+};
 
 const renderDate = (res) => {
   res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/plain; charset=utf-8'
   });
   res.write(`Current time: ${currentTime()}`);
   res.end();
-  
+
   isRunning = false;
   console.log('Sent content to client.');
 };
@@ -37,14 +37,14 @@ const delay = () => {
     console.log(currentTime());
     delay();
   }, config.delta * 1000);
-}
+};
 
 console.log(`Starting server with timeout: ${config.timeout}s, delta: ${config.delta}s. 
   Usage:
   -t: set timeout for request
   -d: delta time for time output`);
 
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
   console.log('Client sent request.');
   isRunning = true;
   setTimeout(renderDate.bind(this, res), config.timeout * 1000);
