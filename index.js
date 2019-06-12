fb02fb64eb55e7ca1f9cff5c424e267546c93799
Alex <slicer256@gmail.com>
2019-06-12 15:20:37 +0300

    basic http server

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-var http = require('http');
+const http = require('http');
+var argv = require('minimist')(process.argv.slice(2));
+
+const config = {
+  timeout: argv.t || 5,
+  delta: argv.d || 1,
+}
+
+const currentTime = () => {
+  time = new Date(new Date().toUTCString());
+  return time;
+}
+
+const renderDate = (res) => {
+  res.writeHead(404, {
+      'Content-Type': 'text/plain; charset=utf-8'
+  });
+  res.write(`Current time: ${currentTime()}`);
+  res.end();
+};
+
+console.log(`Starting server with timeout: ${config.timeout}s, delta: ${config.delta}s. 
+  Usage:
+  -t: set timeout for request
+  -d: delta time for time output`);
+
 http.createServer(function(req, res) {
-  console.log('Time server running');
+  renderDate(res);
 }).listen(8080);
